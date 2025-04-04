74f6713c7d689e2ee5e8f7e4fc216a8275f7eebe
jonesedem <jonesedem1@gmail.com>
2025-04-04 08:11:06 -0700

    Edited 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - prints a given phrase
+ * Return: 0 if exsited properly, non-zero otherwise
+ */
+int main(void)
+{
     puts("Programming is like building a multilingual puzzle");
-    return 0;  // Program ends with value 0
+    return 0;
 }
