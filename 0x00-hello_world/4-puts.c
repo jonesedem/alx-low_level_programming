4a999eb0c224e1b291b61a9a47bc6a69b6864892
jonesedem <jonesedem1@gmail.com>
2025-04-10 03:44:22 -0700

    edited task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 /**
  * main - prints a given phrase
- * Return: 0 if exsited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
