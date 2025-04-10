d3fea0030c18d60a59f7a1c4d1fa1eab62ec9f88
jonesedem <jonesedem1@gmail.com>
2025-04-10 03:33:01 -0700

    edited task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  * main - prints a given phrase
  * Return: 0 if exsited properly, non-zero otherwise
  */
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
