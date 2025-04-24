e5d8d8f3f9d3c5d8c6f243bc46c409e065a4e339
jonesedem <jonesedem1@gmail.com>
2025-04-24 05:29:01 -0700

    5-sign.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-void print_sign(int n);
+int print_sign(int n);
 int _abs(int n);
 void print_last_digit(int n);
 void jack_bauer(void);
