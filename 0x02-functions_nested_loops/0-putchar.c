f88770dc2ced61e1626224925c4ba8b46aa720da
jonesedem <jonesedem1@gmail.com>
2025-04-24 02:20:05 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void) {
+    _putchar('H');
+    _putchar('e');
+    _putchar('l');
+    _putchar('l');
+    _putchar('o');
+    _putchar(' ');
+    _putchar('W');
+    _putchar('o');
+    _putchar('r');
+    _putchar('l');
+    _putchar('d');
+    _putchar('!');
+    _putchar('\n'); /* Print a newline */
+    return (0);
+}
