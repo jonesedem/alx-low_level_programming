12b8afc948485fb41c1fefd6d8f02cb9dd76873f
jonesedem <jonesedem1@gmail.com>
2025-04-25 08:44:03 -0700

    fix: corrected leap year logic in print_remaining_days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 void print_remaining_days(int month, int day, int year)
 {
 	/* Correct leap year check */
-	if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 		/* Adjust for leap year if after Feb 29 */
 		if (month > 2 && day >= 60)
