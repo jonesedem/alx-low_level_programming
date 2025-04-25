6899fed13bad47bfb9d730cf1a01aae04090e7b8
jonesedem <jonesedem1@gmail.com>
2025-04-25 01:47:45 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+    int count;
+    unsigned long fib1 = 1, fib2 = 2, next_fib;
+
+    printf("%lu, %lu", fib1, fib2);
+
+    for (count = 3; count <= 98; count++)
+    {
+        next_fib = fib1 + fib2;
+        printf(", %lu", next_fib);
+        fib1 = fib2;
+        fib2 = next_fib;
+    }
+
+    printf("\n");
+    return (0);
+}
