584f9b7c8028191534b6b604e5f55018460a0321
jonesedem <jonesedem1@gmail.com>
2025-04-30 05:49:00 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-aligned triangle of size 'size'
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int row, space, hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		for (space = size - row; space > 0; space--)
+			_putchar(' ');
+
+		for (hash = 1; hash <= row; hash++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
+
