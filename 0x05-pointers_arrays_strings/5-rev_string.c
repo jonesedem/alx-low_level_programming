78ce106cb823d230eb529b9376a1dbcdc47e00e3
jonesedem <jonesedem1@gmail.com>
2025-05-07 05:30:52 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string to be reversed
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char temp;
+
+	while (s[j] != '\0')
+		j++;
+
+	j--; /* Move to last valid character */
+
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+}
